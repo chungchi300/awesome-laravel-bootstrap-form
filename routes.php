c781f6ec5664e89be5793a022760e8f977c1a045
Astri Jeff Chung <jeffchung@astri.com>
2017-02-16 21:40:42 +0800

    jeff

--- routes.php
+++ routes.php
@@ -6,5 +6,5 @@
 
 Route::get('/jeffchung-awesome', function () {
     //  $user = new App\User();
-    return 'welcomdddssssssssssse in github';
+    return 'welcomdddssssssssssse in github and local path fuck me yeah!!!!!!!!!!!!!!';
 });
